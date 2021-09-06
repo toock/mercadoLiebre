af6103d08698452849bb64ad468eb666f703aca5
toock <santiagohvitali@gmail.com>
2021-09-06 19:50:05 -0300

    primer comit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+app.use('/', express.static(__dirname + '/public'));
+
+//app.listen(3000, ()=>{
+//    console.log('Servidor funcionando en http://localhost:3000');
+//});
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('servidor corriendo en el puerto 3000')
+})
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
